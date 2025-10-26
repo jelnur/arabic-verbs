59027c6a93e3fe5f17292be406f1ec72d11aa82d
Elnur <elnur.j@gmail.com>
2025-10-26 15:01:03 +0400

    hide person column if show zamir is selected

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -113,7 +113,7 @@ export default function Home() {
       <table className={styles.verbTable}>
         <thead>
           <tr>
-            <th>شَخْصٌ</th>
+            {!showPronouns && <th>شَخْصٌ</th>}
             <th>مُفْرَدٌ</th>
             <th>تَثْنِيَةٌ</th>
             <th>جَمْعٌ</th>
@@ -135,9 +135,11 @@ export default function Home() {
                 data-person={personNumber}
                 className={isFirstInSection ? styles.sectionStart : ''}
               >
-                <td className={isFirstInSection ? styles.sectionStart : ''}>
-                  {personLabels[person]}
-                </td>
+                {!showPronouns && (
+                  <td className={isFirstInSection ? styles.sectionStart : ''}>
+                    {personLabels[person]}
+                  </td>
+                )}
                 <td>
                   {showPronouns && personPronouns[person]?.[0] && (
                     <span className={styles.pronoun}>{personPronouns[person][0]} </span>
